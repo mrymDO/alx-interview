2a8dcabbb1f4993ad551101242b37d5e615e7a60
Meryeme <dobli.meryeme@gmail.com>
2023-12-21 11:40:53 -0800

    fix

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,8 +4,13 @@
 import re
 import sys
 
+
 def parse_log_entries():
+    """log parsing"""
+
     total_file_size = 0
+
+    lines_processed = 0
 
     status_code_count = {
         200: 0, 301: 0, 400: 0,
@@ -27,7 +32,8 @@ def parse_log_entries():
                 total_file_size += int(file_size)
                 status_code_count[int(status_code)] += 1
 
-                if total_file_size % 10 == 0:
+                lines_processed += 1
+                if lines_processed % 10 == 0:
                     print(f"File size: {total_file_size}")
                     for code in sorted(status_code_count):
                         print(f"{code}: {status_code_count[code]}")
@@ -37,6 +43,7 @@ def parse_log_entries():
         print(f"File size: {total_file_size}")
         for code in sorted(status_code_count):
             print(f"{code}: {status_code_count[code]}")
+
 
 if __name__ == "__main__":
     parse_log_entries()
