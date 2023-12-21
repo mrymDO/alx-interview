7dc04fc39281f4b09ca7031732fdec9e5effd39c
Meryeme <dobli.meryeme@gmail.com>
2023-12-21 12:52:40 -0800

    test

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,52 +1,42 @@
 #!/usr/bin/python3
-"""Log parsing"""
-
-import re
+"""Reads stdin line by line and computes metrics."""
 import sys
-import traceback
-
-
-def parse_log_entries():
-    """log parsing"""
-
-    total_file_size = 0
-
-    lines_processed = 0
-
-    status_code_count = {
-        '200': 0, '301': 0, '400': 0,
-        '401': 0, '403': 0, '404': 0,
-        '405': 0, '500': 0
-    }
-
-    pattern = re.compile(
-        r'^(\d+\.\d+\.\d+\.\d+) - \[([^\]]+)\] '
-        r'"GET /projects/260 HTTP/1.1" (\d+) (\d+)$'
-    )
-
-    try:
-        for line in sys.stdin:
-            match = pattern.match(line.strip())
-
-            if match:
-                ip_address, date, status_code, file_size = match.groups()
-                total_file_size += int(file_size)
-                status_code_count[status_code] += 1
-
-                lines_processed += 1
-                if lines_processed % 10 == 0:
-                    print(f"File size: {total_file_size}")
-                    for code in sorted(status_code_count):
-                        print(f"{code}: {status_code_count[code]}")
-    except KeyboardInterrupt:
-        raise
-    except BrokenPipeError:
-        pass
-    finally:
-        print(f"File size: {total_file_size}")
-        for code in sorted(status_code_count):
-            print(f"{code}: {status_code_count[code]}")
 
 
 if __name__ == "__main__":
-    parse_log_entries()
+
+    def print_stat(status_codes, file_size):
+        """Prints File size and status code count."""
+        print("File size: {}".format(file_size))
+        for k, v in sorted(status_codes.items()):
+            if v != 0:
+                print("{}: {}".format(k, v))
+
+    file_size, count = 0, 0
+    status_codes = {
+        "200": 0,
+        "301": 0,
+        "400": 0,
+        "401": 0,
+        "403": 0,
+        "404": 0,
+        "405": 0,
+        "500": 0,
+    }
+
+    try:
+        for line in sys.stdin:
+            data = line.split(" ")
+            try:
+                file_size += int(data[-1])
+                if data[-2] in status_codes.keys():
+                    count += 1
+                    status_codes[data[-2]] += 1
+                    if (count % 10) == 0:
+                        print_stat(status_codes, file_size)
+            except:
+                continue
+        print_stat(status_codes, file_size)
+    except KeyboardInterrupt:
+        print_stat(status_codes, file_size)
+        raise
