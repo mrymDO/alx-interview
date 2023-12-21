5160e21cba8e5432e67f521e79e27c260021116e
Meryeme <dobli.meryeme@gmail.com>
2023-12-21 11:33:00 -0800

    task

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,37 +1,42 @@
 #!/usr/bin/python3
 """Log parsing"""
 
-import sys
 import re
+import sys
 
-total_file_size = 0
+def parse_log_entries():
+    total_file_size = 0
 
-status_code_count = {
-    200: 0, 301: 0, 400: 0,
-    401: 0, 403: 0, 404: 0,
-    405: 0, 500: 0
-}
+    status_code_count = {
+        200: 0, 301: 0, 400: 0,
+        401: 0, 403: 0, 404: 0,
+        405: 0, 500: 0
+    }
 
-pattern = re.compile(
-    r'^(\d+\.\d+\.\d+\.\d+) - \[([^\]]+)\] '
-    r'"GET /projects/260 HTTP/1.1" (\d+) (\d+)$'
-)
+    pattern = re.compile(
+        r'^(\d+\.\d+\.\d+\.\d+) - \[([^\]]+)\] '
+        r'"GET /projects/260 HTTP/1.1" (\d+) (\d+)$'
+    )
 
-try:
-    for line in sys.stdin:
-        match = pattern.match(line.strip())
+    try:
+        for line in sys.stdin:
+            match = pattern.match(line.strip())
 
-        if match:
-            ip_adress, date, status_code, file_size = match.groups()
-            total_file_size += int(file_size)
-            status_code_count[int(status_code)] += 1
+            if match:
+                ip_address, date, status_code, file_size = match.groups()
+                total_file_size += int(file_size)
+                status_code_count[int(status_code)] += 1
 
-            if total_file_size % 10 == 0:
-                print(f"File size: {total_file_size}")
-                for code in sorted(status_code_count):
-                    print(f"{code}: {status_code_count[code]}")
-                print()
-except KeyboardInterrupt:
-    print(f"File size: {total_file_size}")
-    for code in sorted(status_code_count):
-        print(f"{code}: {status_code_count[code]}")
+                if total_file_size % 10 == 0:
+                    print(f"File size: {total_file_size}")
+                    for code in sorted(status_code_count):
+                        print(f"{code}: {status_code_count[code]}")
+                    print()
+
+    except KeyboardInterrupt:
+        print(f"File size: {total_file_size}")
+        for code in sorted(status_code_count):
+            print(f"{code}: {status_code_count[code]}")
+
+if __name__ == "__main__":
+    parse_log_entries()
