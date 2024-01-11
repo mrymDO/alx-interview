9c6c2004b313a14c7a87a48b67ec33cec32fb245
Meryeme <dobli.meryeme@gmail.com>
2024-01-11 13:25:33 -0800

    fix

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -15,9 +15,30 @@ def is_safe(placed_queens, row, col):
     return True
 
 
+def print_solution(placed_queens):
+    """Print the current solution in the specified format"""
+    print("[", end="")
+    for cord in placed_queens:
+        print("[{}, {}]".format(cord[0], cord[1]), end=" ")
+    print("\b\b]]")
+
+
+def solve_nqueens(n, row, placed_queens, solutions):
+    """Recursively solve N-Queens"""
+    if row == n:
+        print_solution(placed_queens)
+        return
+
+    for col in range(n):
+        if is_safe(placed_queens, row, col):
+            placed_queens.append([row, col])
+            solve_nqueens(n, row + 1, placed_queens, solutions)
+            placed_queens.pop()
+
+
 if __name__ == '__main__':
     if len(sys.argv) != 2:
-        print("Usage: nqueens N")
+        print("Usage: {} N".format(sys.argv[0]))
         sys.exit(1)
 
     try:
@@ -31,60 +52,4 @@ if __name__ == '__main__':
         exit(1)
 
     solutions = []
-    placed_queens = []  # coordinates format [row, column]
-    stop = False
-    r = 0
-    c = 0
-
-    while r < n:
-        goback = False
-
-        while c < n:
-            if not is_safe(placed_queens, r, c):
-                if c == n - 1:
-                    goback = True
-                    break
-                c += 1
-                continue
-
-            cords = [r, c]
-            placed_queens.append(cords)
-
-            if r == n - 1:
-                solutions.append(placed_queens[:])
-                for cord in placed_queens:
-                    if cord[1] < n - 1:
-                        r = cord[0]
-                        c = cord[1]
-                for i in range(n - r):
-                    placed_queens.pop()
-                if r == n - 1 and c == n - 1:
-                    placed_queens = []
-                    stop = True
-                r -= 1
-                c += 1
-            else:
-                c = 0
-            break
-
-        if stop:
-            break
-
-        if goback:
-            r -= 1
-            while r >= 0:
-                c = placed_queens[r][1] + 1
-                del placed_queens[r]
-                if c < n:
-                    break
-                r -= 1
-            if r < 0:
-                break
-            continue
-        r += 1
-
-    for idx, val in enumerate(solutions):
-        if idx == len(solutions) - 1:
-            print(val, end='')
-        else:
-            print(val)
+    solve_nqueens(n, 0, [], solutions)
