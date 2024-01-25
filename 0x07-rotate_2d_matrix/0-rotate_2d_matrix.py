77740564fe153e27dae34684dff657fb5e06d4af
Meryeme <dobli.meryeme@gmail.com>
2024-01-25 15:53:06 -0800

    task

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,16 @@
+#!/user.bin/python3
+"""Rotate 2D Matrix
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """rotate 2D matrix 90 degrees clockwise in-place.
+    """
+    n = len(matrix)
+
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        for i in range(n):
+            matrix[i].reverse()
