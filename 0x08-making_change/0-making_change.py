f83ae9dcc34a1dc1371b6663de688f28bf16cf39
Meryeme <dobli.meryeme@gmail.com>
2024-02-01 12:29:59 -0800

    fix

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -4,16 +4,21 @@
 
 
 def makeChange(coins, total):
-    """making change"""
+    """number of coins needed to  meet the total
+    """
     if total <= 0:
         return 0
+
+    if len(coins) is 0:
+        return -1
+
+    coins = sorted(coins)
 
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
 
-    for i in range(1, total + 1):
-        for coin in coins:
-            if i >= coin:
+    for coin in coins:
+        for i in range(coin, total + 1):
                 dp[i] = min(dp[i], dp[i - coin] + 1)
 
     return dp[total] if dp[total] != float('inf') else -1
