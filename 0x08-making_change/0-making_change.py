d4b7ed1587a0a75a040d0d66af192d6dccba1df2
Meryeme <dobli.meryeme@gmail.com>
2024-02-02 01:57:49 -0800

    fx

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -18,6 +18,8 @@ def makeChange(coins, total):
     dp[0] = 0
 
     for coin in coins:
+        if coin > total:
+            break
         for i in range(coin, total + 1):
                 dp[i] = min(dp[i], dp[i - coin] + 1)
 
