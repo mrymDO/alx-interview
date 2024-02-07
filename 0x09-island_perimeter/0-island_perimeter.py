b27069b1933b0cf9f68904da3386264c80453ca5
Meryeme <dobli.meryeme@gmail.com>
2024-02-07 07:05:07 -0800

    island perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Island perimeter
+"""
+
+def island_perimeter(grid):
+    perimeter = 0
+
+    if not grid or not grid[0]:
+        return perimeter
+
+    rows = len(grid)
+    cols = len(grid[0])
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
