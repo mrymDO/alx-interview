f47413c1ece19801891f58a30f9706d9e29553f0
Meryeme <dobli.meryeme@gmail.com>
2024-02-15 13:27:13 -0800

    fix

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,63 +1,35 @@
 #!/usr/bin/python3
-"""Prime Game
+"""
+Prime game
 """
 
 
+def generate_primes(limit):
+    """Generate a list of primes"""
+    primes = []
+    is_prime = [True] * (limit + 1)
+
+    for num in range(2, limit + 1):
+        if is_prime[num]:
+            primes.append(num)
+            for multiple in range(num * num, limit + 1, num):
+                is_prime[multiple] = False
+
+    return primes
+
+
 def isWinner(x, nums):
-    """Prime game winner"""
-
-    def is_prime(num):
-        """check if a number is a prime"""
-        if num < 2:
-            return False
-        for i in range(2, int(num**0.5) + 1):
-            if num % i == 0:
-                return False
-        return True
-
-    memo_primes = {}
-
-    def get_primes_up_to_n(n):
-        """get list of prime numbers up to n"""
-        if n not in memo_primes:
-            sieve = [True] * (n + 1)
-            sieve[0] = sieve[1] = False
-            for p in range(2, int(n**0.5) + 1):
-                if sieve[p]:
-                    for i in range(p * p, n + 1, p):
-                        sieve[i] = False
-            primes = [p for p in range(2, n + 1) if sieve[p]]
-            memo_primes[n] = primes
-        return memo_primes[n]
-
-    def optimal_moves(nums):
-        """find optimal moves"""
-        primes = get_primes_up_to_n(max(nums))
-        for prime in primes:
-            if prime in nums:
-                return [num for num in nums if num % prime != 0]
-
-    maria_wins = 0
-    ben_wins = 0
-
-    for round_num in range(x):
-        current_round = nums[round_num]
-        current_nums = list(range(1, current_round + 1))
-
-        while current_nums:
-            current_nums = optimal_moves(current_nums)
-            if not current_nums:
-                ben_wins += 1
-                break
-
-            current_nums = optimal_moves(current_nums)
-            if not current_nums:
-                maria_wins += 1
-                break
-
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif maria_wins < ben_wins:
-        return "Ben"
-    else:
+    """Determine the winner"""
+    if x is None or nums is None or x == 0 or not nums:
         return None
+
+    maria_wins, ben_wins = 0, 0
+
+    for limit in nums:
+        primes = generate_primes(limit)
+        winner = 'Ben' if len(primes) % 2 == 0 else 'Maria'
+        ben_wins += (winner == 'Ben')
+        maria_wins += (winner == 'Maria')
+
+    return 'Maria' if maria_wins > ben_wins \
+        else 'Ben' if ben_wins > maria_wins else None
