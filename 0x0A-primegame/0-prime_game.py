f4113ba47e25cc928224b3e974349daf54803231
Meryeme <dobli.meryeme@gmail.com>
2024-02-15 13:08:39 -0800

    test

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,57 +1,36 @@
 #!/usr/bin/python3
-"""Prime Game
+"""
+define is winner
 """
 
 
+def getPrimes(n):
+    """ list of primes"""
+    prime = []
+    sieve = [True] * (n + 1)
+    for p in range(2, n + 1):
+        if (sieve[p]):
+            prime.append(p)
+            for i in range(p, n + 1, p):
+                sieve[i] = False
+    return prime
+
+
 def isWinner(x, nums):
-    """Prime game winner"""
-
-    def is_prime(num):
-        """check if a number is a prime"""
-        if num < 2:
-            return False
-        for i in range(2, int(num**0.5) + 1):
-            if num % i == 0:
-                return False
-        return True
-
-    memo_primes = {}
-
-    def get_primes_up_to_n(n):
-        """get list of prime numbers up to n"""
-        if n not in memo_primes:
-            primes = [i for i in range(2, n + 1) if is_prime(i)]
-            memo_primes[n] = primes
-        return memo_primes[n]
-
-    def optimal_moves(nums):
-        """find optimal moves"""
-        primes = get_primes_up_to_n(max(nums))
-        for prime in primes:
-            if prime in nums:
-                return [num for num in nums if num % prime != 0]
-
-    maria_wins = 0
-    ben_wins = 0
-
-    for round_num in range(x):
-        current_round = nums[round_num]
-        current_nums = list(range(1, current_round + 1))
-
-        while current_nums:
-            current_nums = optimal_moves(current_nums)
-            if not current_nums:
-                ben_wins += 1
-                break
-
-            current_nums = optimal_moves(current_nums)
-            if not current_nums:
-                maria_wins += 1
-                break
-
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif maria_wins < ben_wins:
-        return "Ben"
-    else:
+    """
+        define winner
+    """
+    if x is None or nums is None or x == 0 or nums == []:
         return None
+    maria_count = ben_count = 0
+    for i in range(x):
+        prime = getPrimes(nums[i])
+        if len(prime) % 2 == 0:
+            ben_count += 1
+        else:
+            maria_count += 1
+    if maria_count > ben_count:
+        return 'Maria'
+    elif ben_count > maria_count:
+        return 'Ben'
+    return None
